52d3322d666eb0b25fd96cb32c68f646f6edc184
Jaeyeon Kim <0310kjy@gmail.com>
2023-01-03 18:10:09 +0900

    Update Items.tsx

--- src/screens/Items.tsx
+++ src/screens/Items.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, ScrollView, Image, SafeAreaView, Text, TextInput, TouchableOpacity,
+import { View, Text, TouchableOpacity,
         NativeSyntheticEvent, TextInputChangeEventData, Button } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../types/RootStackParamList';
@@ -7,7 +7,6 @@ import styled from 'styled-components';
 import * as S from '../components/CommonComponents';
 import { useRecoilState } from 'recoil';
 import { userState } from '../atoms/userState';
-import { loadAsync } from 'expo-font';
 import MyItemsPage from './MyItems';
 import YourItemsPage from './YourItems';
 
